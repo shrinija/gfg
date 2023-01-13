ac69714c665845563e2b6ff71b64bffb104b59c8
shrinija <98155154+shrinija@users.noreply.github.com>
2023-01-13 19:04:47 +0530

    Update Preorder Traversal.cpp

--- Preorder Traversal.cpp
+++ Preorder Traversal.cpp
@@ -2,6 +2,8 @@
 // see first and second ans........know what is the difference and why.......?
 
 //.....................................................................................................................
+//recursive
+
 
 vector<int>& helper(Node*root ,vector<int>&v)                                      //  see   &    and know why...
 {
@@ -21,6 +23,9 @@ vector <int> preorder(Node* root)
 }
 
 //............................................................................................................
+//recursive
+
+
 void helper(Node*root ,vector<int>&v)
 {
  if(root==NULL)
@@ -38,5 +43,39 @@ vector <int> preorder(Node* root)
   helper(root,v);
   return v;
 }
+
 //...............................................................................................................
 
+//itetrative
+
+// see how smartly we are using stack..think like this...like what we can do .. what changes to be made to reach to our result.. how to use available data structures
+
+vector <int> preorder(Node* root)
+{
+  // Your code here
+  vector<int> v;
+  stack<Node*> st;
+  if(root)
+  {
+    st.push(root);
+  }
+  
+  while(!st.empty())
+  {
+      Node*top = st.top();
+      st.pop();
+      v.push_back(top->data);
+      
+      if(top->right != NULL)
+      {
+          st.push(top->right);
+      }
+      if(top->left != NULL)
+      {
+          st.push(top->left);
+      }
+  }
+ return v;
+}
+
+//..........................................................................................................................
